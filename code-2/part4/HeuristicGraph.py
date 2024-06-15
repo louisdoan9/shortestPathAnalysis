4ae7e3083d50ada0ff8b55b12e08eb12b50c3cb3
Louis Doan <louisdoan9@gmail.com>
2024-06-15 18:46:49 -0400

    Delete irrelevant files

--- code-2/part4/HeuristicGraph.py
+++
@@ -1,9 +0,0 @@
-from WeightedGraph import WeightedGraph
-
-class HeuristicGraph(WeightedGraph):
-    
-    def __init__(self, heuristic):
-        self.heuristic = heuristic
-
-    def get_heuristic(self):
-        return self.heuristic
--- code-2/part4/SPAlgorithm.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-class SPAlgorithm(ABC):
-    
-    @abstractmethod 
-    def calc_sp(graph, source, dest):
-        pass
